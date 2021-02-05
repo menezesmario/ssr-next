22ca80b06553b8f9a6278a9f22bc20f9dee1fafb
Mário Menezes <mario082@gmail.com>
2021-02-05 19:01:38 -0300

    ssr com api ok

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import "isomorphic-fetch";
 
 export default class Home extends Component {
+    static getInitialProps = async () => {
+        const response = await fetch(
+            "https://api.github.com/orgs/rocketseat/repos"
+        );
+
+        return { repositores: await response.json() };
+    };
+
     render() {
         return (
-            <h1> Hellow World </h1>
-        )
+            <div>
+                {this.props.repositores.map(repo => (
+                    <h1 key={repo.id}>{repo.name}</h1>
+                ))}
+            </div>
+        );
     }
 }
